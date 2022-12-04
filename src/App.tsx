c3f5fb20dce1d08939da847dc65e08ade42464fb
Mauricio Carvalho <mcarvalhojb@gmail.com>
2022-12-04 09:40:30 -0300

    add: Footer, Navbar and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './componentes/estaticos/navbar/Navbar';
-import { Home } from './paginas/home/Home';
-import { About } from './paginas/about/About'
-import LabelBottomNavigation from './componentes/estaticos/footer/Footer';
-import Footer from './componentes/estaticos/footer/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from "./components/static/footer/Footer";
+import Navbar from "./components/static/navbar/Navbar";
+import Home from "./pages/home/Home";
+import './App.css'
+import Login from "./pages/login/Login";
+
 
 function App() {
   return (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/Home' element={<Home />} />
-      <Route path='/About' element={<About />} />
-    </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
       <Footer />
-  </Router>
+    </Router>
+
   );
 }
 
